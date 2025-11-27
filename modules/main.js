7d062eee550440c13b188f654f0cf9a1a470a77c
velpuri_rajendra <velpurirajendra@example.com>
2025-11-27 19:59:40 +0530

    Initial commit

---
+++ modules/main.js
@@ -0,0 +1,5 @@
+// console.log(add(100, 200))  //300
+// console.log(sub(100, 200))  //-100
+
+import { add, sub } from "./utils";
+console.log(add(1, 1))
---
+++ pure react with jsx/foodItems.js
@@ -0,0 +1,42 @@
+var pizzaItems = [
+  {
+    id: 1,
+    name: "Margherita",
+    description: "Classic delight with 100% real mozzarella cheese",
+    price: 8.99,
+    size: ["Small", "Medium", "Large"],
+    vegetarian: true
+  },
+  {
+    id: 2,
+    name: "Pepperoni",
+    description: "Loaded with pepperoni and cheese",
+    price: 10.99,
+    size: ["Medium", "Large"],
+    vegetarian: false
+  },
+  {
+    id: 3,
+    name: "BBQ Chicken",
+    description: "Grilled chicken with smoky BBQ sauce",
+    price: 11.99,
+    size: ["Small", "Medium", "Large"],
+    vegetarian: false
+  },
+  {
+    id: 4,
+    name: "Veggie Supreme",
+    description: "Loaded with fresh vegetables and cheese",
+    price: 9.99,
+    size: ["Small", "Medium"],
+    vegetarian: true
+  },
+  {
+    id: 5,
+    name: "Hawaiian",
+    description: "Ham and pineapple on a cheese base",
+    price: 10.49,
+    size: ["Medium", "Large"],
+    vegetarian: false
+  }
+];
